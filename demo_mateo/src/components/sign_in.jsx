9a9e408bdf2b1850567b45ba99e819ed82a42763
Mateo Caso <67202211+CasoMateo@users.noreply.github.com>
2021-10-28 20:10:23 +0000

    Sign-in component

---
+++ demo_mateo/src/components/sign_in.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'; 
+
+class AutoForm extends Component {
+  
+  state = {};
+
+  styles = { 
+
+  }
+
+  render() {
+    return {
+
+      
+      
+    }
+
+  }
+}
+
+export default AutoForm;
